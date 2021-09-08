e79151505736db38dd6d0c887f86a1b04ad19010
Eddy Jaga <82800286+eddyjaga@users.noreply.github.com>
2021-09-08 23:13:20 +1000

    Shifting letters
    This is a problem 848 on leetcode

---
+++ ShiftingLetters.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/shifting-letters/
+class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        int shift = 0;
+        char[] str = s.toCharArray();
+        for(int i=str.length-1; i>=0; i--){
+            shift = (shift+shifts[i])%26;
+            str[i] = (char)((str[i]-'a'+shift) % 26 +'a');
+        }
+        return new String(str);
+    }
+}
